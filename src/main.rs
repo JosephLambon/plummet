82c739f5107dc687a4916ae18f0fcddcf23a6865
Joe Lambon <joe.lambon25@gmail.com>
2026-03-22 16:21:45 +0000

    Update LimitOrder struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::collections::BTreeMap;
 
 use rust_decimal::{Decimal, dec};
 
+use chrono::prelude::*;
+
 fn main() {
     println!("\nWelcome. This is Joe's Order Book.");
     println!("==================================\n\n");
@@ -9,36 +11,38 @@ fn main() {
     let mut orders: BTreeMap<Decimal, LimitOrder> = BTreeMap::new();
 
     let order1 = LimitOrder {
-        stock: String::from("GOOGL"),
+        time_placed: Local::now(),
+        stock_symbol: String::from("GOOGL"),
         limit_price: dec!(1234.5600),
         quantity: dec!(50),
-        action: LimitOrderAction::Buy,
+        side: LimitOrderAction::Buy,
     };
     let order2 = LimitOrder {
-        stock: String::from("AAPL"),
+        time_placed: Local::now(),
+        stock_symbol: String::from("AAPL"),
         limit_price: dec!(1123.5698),
         quantity: dec!(50),
-        action: LimitOrderAction::Sell,
+        side: LimitOrderAction::Sell,
     };
 
     orders.insert(order1.limit_price, order1);
     orders.insert(order2.limit_price, order2);
 
-    let mut orders_iterator = orders.iter().enumerate();
-
-    while let Some((index, order)) = orders_iterator.next() {
+    for (index, order) in orders.iter().enumerate() {
         println!(
             "Order {index}: {:?} {} shares of {} at limit price £{}",
-            order.1.action, order.1.quantity, order.1.stock, order.1.limit_price
+            order.1.side, order.1.quantity, order.1.stock_symbol, order.1.limit_price
         );
+        println!("Time placed: {}\n", order.1.time_placed);
     }
 }
 
 pub struct LimitOrder {
-    stock: String,
+    time_placed: DateTime<Local>,
+    stock_symbol: String,
     limit_price: Decimal,
     quantity: Decimal,
-    action: LimitOrderAction,
+    side: LimitOrderAction,
 }
 
 #[derive(Debug)]
